85adcf8d916a6c0d2be7cf5f20a07e8956413487
Jvdboss7 <jaydeepdixit2@gmail.com>
2022-09-03 23:52:45 +0530

    dataIngestion.py added

--- src/dataIngestion.py
+++ src/dataIngestion.py
@@ -1,4 +1,4 @@
-from src.utils.all_utils import read_yaml
+from src.utils.all_utils import read_yaml,create_directory
 import argparse
 import pandas as pd
 import os 
@@ -7,10 +7,21 @@ def get_data(config_path):
     config = read_yaml(config_path)
 
     remote_data_path = config["data_source"]
-    df = pd.read_csv(remote_data_path)
+    df = pd.read_csv(remote_data_path,sep=",")
 
     # Save the dataset in the local directory 
     # Create path to directory: artifacts/raw_local_dir/data.csv
+    artifacts_dir = config["artifacts"]['artifacts_dir']
+    raw_local_dir = config["artifacts"]['raw_local_dir']
+    raw_local_file = config["artifacts"]['raw_local_file']
+
+    raw_local_dir_path = os.path.join(artifacts_dir,raw_local_dir)
+
+    create_directory(dirs=[raw_local_dir_path])
+
+    raw_local_file_path = os.path.join(raw_local_dir_path,raw_local_file)
+
+    df.to_csv(raw_local_file_path,sep=",",index=False)    
 
     
 
@@ -18,5 +29,8 @@ if __name__=='__main__':
     args = argparse.ArgumentParser()
 
     args.add_argument("--config","-c",default="config/config.yaml")
+
     parsed_args = args.parse_args()
+
     get_data(config_path=parsed_args.config)
+    
