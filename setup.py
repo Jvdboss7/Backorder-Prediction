68f25d480d6bee8fc7a50090e7862809d5a30fb9
Jvdboss7 <jaydeepdixit2@gmail.com>
2022-09-03 15:02:57 +0530

    setup.py added

---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+
+with open("README.md","r",encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name = "Backorder",
+    version = "0.0.1",
+    author = "Jaydeep Dixit",
+    description = "Backorder prediction package",
+    long_description = long_description,
+    url ="https://github.com/Jvdboss7/Backorder-Prediction",
+    author_email="jaydeep@ineuron.ai",
+    packages = ["src"],
+    python_requires = ">=3.7",
+    install_requires = [
+        'dvc',
+        'pandas',
+        'scikit-learn'
+    ]
+)
