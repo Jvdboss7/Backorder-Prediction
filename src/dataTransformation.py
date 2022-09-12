505840afb673f4143f78e124f9da18eaa4a525b1
Jvdboss7 <jaydeepdixit2@gmail.com>
2022-09-12 17:12:11 +0530

    dataTransformationPreparation

---
+++ src/dataTransformation.py
@@ -0,0 +1,86 @@
+
+from src.utils.all_utils import read_yaml,create_directory
+import argparse
+import pandas as pd
+pd.options.mode.chained_assignment = None
+from sklearn.impute import SimpleImputer
+from sklearn.preprocessing import StandardScaler
+from sklearn.decomposition import PCA
+import numpy as np
+import os 
+
+def get_data(config_path):
+    config = read_yaml(config_path)
+
+    remote_data_path = config["data_source"]
+    df = pd.read_csv(remote_data_path,sep=",")
+
+    # Save the dataset in the local directory 
+    # Create path to directory: artifacts/raw_local_dir/data.csv
+    artifacts_dir = config["artifacts"]['artifacts_dir']
+    raw_local_dir = config["artifacts"]['raw_local_dir']
+    raw_local_file = config["artifacts"]['raw_local_file']
+
+
+    raw_local_file_path = os.path.join(artifacts_dir,raw_local_dir,raw_local_file)
+
+    print(raw_local_file_path)
+
+    df = pd.read_csv(raw_local_file_path)
+
+def process(df):
+    """
+    Some strategies adopted:
+    - Binaries were converted from strings ('Yes' and 'No') to 1 and 0.
+    - The attributes related to quantities were normalized (std dev equal to 1)
+    per row. Therefore, parts with different order of magnitudes are 
+    approximated. For example: 1 unit of a expensive machine may be different 
+    from 1 unit of a screw, but if we standard deviate all the quantities we 
+    have, we can get a better proportion of equivalence between those items.
+    - Missing values for lead_time and perf_month_avg were replaced using 
+    series median and mean. 
+    """
+    # Imput missing lines and drop line with problem
+
+    df['lead_time'] = SimpleImputer(strategy='median').fit_transform(
+                                    df['lead_time'].values.reshape(-1, 1))
+    df = df.dropna()
+    for col in ['perf_6_month_avg', 'perf_12_month_avg']:
+        df[col] = SimpleImputer(missing_values=-99).fit_transform(
+                                    df[col].values.reshape(-1, 1))
+    # Convert to binaries
+    for col in ['potential_issue', 'deck_risk', 'oe_constraint', 'ppap_risk',
+               'stop_auto_buy', 'rev_stop', 'went_on_backorder']:
+        df[col] = (df[col] == 'Yes').astype(int)
+    # Normalization    
+    from sklearn.preprocessing import normalize
+    qty_related = ['national_inv', 'in_transit_qty', 'forecast_3_month', 
+                   'forecast_6_month', 'forecast_9_month', 'min_bank',
+                   'local_bo_qty', 'pieces_past_due', 'sales_1_month', 
+                   'sales_3_month', 'sales_6_month', 'sales_9_month',]
+    df[qty_related] = normalize(df[qty_related], axis=1)
+    df = df.astype(float)   
+    return df
+
+new_df=process(df)
+
+def transformation(new_df):
+    # StandardScaler for scaling the values
+    X= df.drop(df.columns[-1], axis = 1)
+    #y = df['went_on_backorder'] 
+    X_std = StandardScaler().fit_transform(X)
+
+    # PCA for dimensionality reduction 
+    dec = PCA(n_components=2)
+    X_reduced = dec.fit_transform(X_std)
+    return X_reduced
+
+
+if __name__=='__main__':
+    args = argparse.ArgumentParser()
+
+    args.add_argument("--config","-c",default="config/config.yaml")
+
+    parsed_args = args.parse_args()
+
+    get_data(config_path=parsed_args.config)
